99734d12d88e8710440605439dbca38d7a0f9cc7
quentindamois <quentin.damois@efrei.net>
2026-01-28 15:08:09 +0100

    Update train_and_register.py
    Change this file to trigger an update

--- ml/train_and_register.py
+++ ml/train_and_register.py
@@ -9,7 +9,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 MODEL_NAME = os.getenv("MODEL_NAME", "churn-model")
 
-# useless update 3
+# useless update 4
 
 def main():
     tracking_uri = os.environ["MLFLOW_TRACKING_URI"]
